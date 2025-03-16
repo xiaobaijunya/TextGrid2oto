879e02cddbe7c1cc21abdaa6813af8cb2a6382d7
Xiaobaijunya <xiaobaijunya@gmail.com>
2025-03-16 22:02:03 +0800

    增加了VV的生成规则，修正了VC的错误生成

--- json2oto.py
+++ json2oto.py
@@ -16,16 +16,18 @@ def presamp_read(presamps_path):
         # 提取 [VOWEL] 部分
         vowel_match = re.search(r'\[VOWEL\](.*?)\[', ini_text, re.DOTALL)
         vowels = vowel_match.group(1).strip()
+        # print(vowels)
         # 提取 [CONSONANT] 部分
         consonant_match = re.search(r'\[CONSONANT\](.*?)\[', ini_text, re.DOTALL)
         consonants = consonant_match.group(1).strip()
-        for vowel in vowels:
-            V.append(vowel.split('=')[0])
-        for consonant in consonants:
-            C.append(consonant.split('=')[0])
+        # print(consonants)
+    for vowel in vowels.split('\n'):
+        V.append(vowel.split('=')[0])
+    for consonant in consonants.split('\n'):
+        C.append(consonant.split('=')[0])
     V = set(V)
     C = set(C)
-    # print(V,C)
+    print(V,C)
     return V,C
 
 def json2cvoto(cv_data,sum):
@@ -91,13 +93,36 @@ def json2vcoto(vc_data,C_V,sum):
         while i < len(sorted_phones) - 1:
             key, cont = sorted_phones[i]
             key1, cont2 = sorted_phones[i + 1]
-            # -CV规则
+            # 0V 1C
+            # CC规则
             if cont['text'] in C_V[1] and cont2['text'] in C_V[1]:
+                print(C_V[1])
+                print(cont['text'] +''+ cont2['text'])
                 i+=1
                 continue
+                # -CV规则
             elif cont['text'] == '-' :
                 i+=1
                 continue
+                #CV规则
+            elif cont['text'] in C_V[1] and cont2['text'] in C_V[0]:
+                i+=1
+                continue
+                #VV规则
+            elif cont['text'] in C_V[0] and cont2['text'] in C_V[0]:
+                phone_name = cont['text'] + ' ' + cont2['text']
+                # autio_name=phone_name,left,fixed,right（负值）,Prevoice,cross
+                left = float(cont["xmin"]) * 1000 + ((float(cont['xmax']) - float(cont['xmin'])) * 1000 / sum[0])
+                # 右线占比
+                right = (float(cont2['xmax']) - float(cont2['xmin'])) * 1000 / sum[2] - left + float(
+                    cont2['xmin']) * 1000
+                Prevoice = float(cont2['xmin']) * 1000 - left / sum[3]
+                # 固定的占比
+                fixed = Prevoice / sum[5]
+
+                cross = Prevoice / sum[4]
+                i += 1
+                oto.append(f"{autio_name}={phone_name},{left},{fixed},-{right},{Prevoice},{cross}\n")
             else:
                 phone_name = cont['text'] +' ' + cont2['text']
                 # autio_name=phone_name,left,fixed,right（负值）,Prevoice,cross
@@ -107,7 +132,7 @@ def json2vcoto(vc_data,C_V,sum):
                 Prevoice =float(cont2['xmin'])*1000-left/ sum[3]
                 # 固定的占比
                 if sum[1] ==0:
-                    fixed = Prevoice
+                    fixed = 0
                 else:
                     fixed = right+(right-Prevoice)/sum[1]
 
@@ -139,13 +164,18 @@ def run(presamp_path,utau_phone_json,word_phone_json,wav_path,cv_sum,vc_sum):
 
 
 if __name__ == '__main__':
-    presamp_path = 'presamp.ini'
-    utau_phone = 'G:/编程/utau自动标注/F3/TextGrid/json/utau_phone.json'
-    word_phone_json = 'G:/编程/utau自动标注/F3/TextGrid/json/word_phone.json'
-    wav_path = 'G:/编程/utau自动标注/F3'
+    # presamp_path = 'presamp.ini'
+    # utau_phone = 'G:/编程/utau自动标注/F3/TextGrid/json/utau_phone.json'
+    # word_phone_json = 'G:/编程/utau自动标注/F3/TextGrid/json/word_phone.json'
+    # wav_path = 'G:/编程/utau自动标注/F3'
+
+    presamp_path = 'E:\\UTAU\\voice\yuezheng_longya\presamp.ini'
+    utau_phone = 'E:\\UTAU\\voice\yuezheng_longya\D4/TextGrid/json/utau_phone.json'
+    word_phone_json = 'E:\\UTAU\\voice\yuezheng_longya\D4/TextGrid/json/word_phone.json'
+    wav_path = 'E:\\UTAU\\voice\yuezheng_longya\D4'
 
     #-CV和CV规则：左线占比,固定的占比,右线占比,预发声不变,交叉占比
     cv_sum = [1,3,1.5,1,2]
-    #VC和VV规则：左线占比,固定的占比,右线占比,预发声不变,交叉占比
-    vc_sum=[3,0,2,1,2]
+    #VC和VV规则：左线占比,固定的占比,右线占比,预发声不变,交叉占比,VV固定占比
+    vc_sum=[3,0,2,1,2,3]
     run(presamp_path,utau_phone,word_phone_json,wav_path,cv_sum,vc_sum)
--- main.py
+++ main.py
@@ -5,47 +5,54 @@ import ds_json2word
 import word2utau_phone
 import json2oto
 import oto
-# nuitka --standalone --onefile --output-filename='TextGrid2oto_v0.1.0' main.py
+# nuitka --standalone --onefile --output-filename=TextGrid2oto_v0.1.1 main.py
 
 if __name__ == '__main__':
-    print('sofa-UTAU自动标注')
-    print('1.生成lab')
-    wav_path=input('请输入wav的路径：')
-    cut=input('请输入分隔符默认为_')
-    if not cut:
-        cut = '_'
-    wavname2lab.run(wav_path,cut)
-    print('2.生成TextGrid')
-    print('需要自己前往sofa生成TextGrid')
-    input('生成完成后,请输入任意键继续')
-    print('3.生成json')
-    TextGrid_path=input('请输入TextGrid的路径：')
-    ds_dict=input('请输入sofa模型对应ds字典的路径：')
-    if not TextGrid_path:
-        TextGrid_path = wav_path+'/TextGrid'
-    if not ds_dict:
-        ds_dict = 'opencpop-extension.txt'
-    TextGrid2ds_json.run(TextGrid_path)
-    ds_json2filter.run(ds_dict,TextGrid_path+'/json/ds_phone.json')
-    print('3.生成word.json')
-    ds_json2word.run(ds_dict,TextGrid_path+'/json/ds_phone_filter.json')
-    print('4.生成utau音素')
-    presamp=input('请输入presamp.ini的路径：')
-    if not presamp:
-        presamp = 'presamp.ini'
-    word2utau_phone.generate_utau_phone(presamp,TextGrid_path+'/json/word_phone.json')
-    print('5.生成utauphone_json')
-    word2utau_phone.generate_utau_phone(presamp,TextGrid_path+'/json/utau_phone.json')
-    print('6.生成oto.ini')
-    #-CV和CV规则：左线占比,固定的占比,右线占比,预发声不变,交叉占比
-    cv_sum = [1,3,1.5,1,2]
-    #VC和VV规则：左线占比,固定的占比,右线占比,预发声不变,交叉占比
-    vc_sum=[3,0,2,1,2]
-    json2oto.run(presamp,TextGrid_path+'/json/utau_phone.json',TextGrid_path+'/json/word_phone.json',wav_path,cv_sum,vc_sum)
-    print('7.合并oto.ini')
-    cv = oto.oto_read(wav_path+'/cv_oto.ini')
-    vc = oto.oto_read(wav_path+'/vc_oto.ini')
-    oto.oto_write(wav_path+'/oto.ini',cv+vc)
-    print('10086.完成！')
-    input('输入任意键退出')
-    quit()
+    try:
+        print('sofa-UTAU自动标注')
+        print('1.生成lab')
+        wav_path=input('请输入wav的路径：')
+        cut=input('请输入分隔符默认为_')
+        if not cut:
+            cut = '_'
+        wavname2lab.run(wav_path,cut)
+        print('2.生成TextGrid')
+        print('需要自己前往sofa生成TextGrid')
+        input('生成完成后,请输入任意键继续')
+        print('3.生成json')
+        TextGrid_path=input('请输入TextGrid的路径：')
+        ds_dict=input('请输入sofa模型对应ds字典的路径：')
+        if not TextGrid_path:
+            TextGrid_path = wav_path+'/TextGrid'
+        if not ds_dict:
+            ds_dict = 'opencpop-extension.txt'
+        TextGrid2ds_json.run(TextGrid_path)
+        ds_json2filter.run(ds_dict,TextGrid_path+'/json/ds_phone.json')
+        print('3.生成word.json')
+        ds_json2word.run(ds_dict,TextGrid_path+'/json/ds_phone_filter.json')
+        print('4.生成utau音素')
+        presamp=input('请输入presamp.ini的路径：')
+        if not presamp:
+            presamp = 'presamp.ini'
+        word2utau_phone.generate_utau_phone(presamp,TextGrid_path+'/json/word_phone.json')
+        print('5.生成utauphone_json')
+        word2utau_phone.generate_utau_phone(presamp,TextGrid_path+'/json/utau_phone.json')
+        print('6.生成oto.ini')
+        # -CV和CV规则：左线占比,固定的占比,右线占比,预发声不变,交叉占比
+        cv_sum = [1, 3, 1.5, 1, 2]
+        # VC和VV规则：左线占比,固定的占比,右线占比,预发声不变,交叉占比,VV固定占比
+        vc_sum = [3, 0, 2, 1, 2, 3]
+        json2oto.run(presamp,TextGrid_path+'/json/utau_phone.json',TextGrid_path+'/json/word_phone.json',wav_path,cv_sum,vc_sum)
+        print('7.合并oto.ini')
+        cv = oto.oto_read(wav_path+'/cv_oto.ini')
+        vc = oto.oto_read(wav_path+'/vc_oto.ini')
+        oto.oto_write(wav_path+'/oto.ini',cv+vc)
+        print('10086.完成！')
+    except Exception as e:
+        import traceback
+        print("\n发生错误：")
+        print(traceback.format_exc())
+        print("请联系开发者检查错误")
+    finally:
+        input('输入任意键退出')
+        quit()
