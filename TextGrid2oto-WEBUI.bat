4201d0a8305ab36574694b5033b9fffb67669722
xiaobaijunya <xiaobaijunya@gmail.com>
2025-04-17 21:21:52 +0800

    更新

--- TextGrid2oto-WEBUI.bat
+++ TextGrid2oto-WEBUI.bat
@@ -1,7 +1,16 @@
 @echo off
+:: Check for admin rights
 
-echo Launching textgrid2oto WebUI ...
 
-.\env\Scripts\python.exe .\WEB-UI.py
+set PYTHON_REL_PATH=.\workenv\python.exe
+set SCRIPT_REL_PATH=WEB-UI.py
 
+set SCRIPT_DIR=%~dp0
+set PYTHON_PATH=%SCRIPT_DIR%%PYTHON_REL_PATH%
+set SCRIPT_PATH=%SCRIPT_DIR%%SCRIPT_REL_PATH%
+
+cd /d %SCRIPT_DIR%
+
+echo Æô¶¯TextGrid2oto-WEBUI
+"%PYTHON_PATH%" "%SCRIPT_PATH%"
 pause
