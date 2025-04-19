54778d3ea50e6860c74c30e2451d4f3e960578a6
xiaobaijunya <xiaobaijunya@gmail.com>
2025-04-19 22:55:53 +0800

    更新支持SOFA

--- WEB-UI.py
+++ WEB-UI.py
@@ -4,38 +4,13 @@ import threading
 import queue
 # nuitka --standalone --windows-icon-from-ico=img/TextGrid2oto.ico --output-dir=output --output-filename=TextGrid2oto-WEBUI WEB-UI.py
 
-# 创建一个队列用于线程间通信
-file_dialog_queue = queue.Queue()
-
 def run_app():
     app.run()
-
-# 修改 app.py 中的 select_file 函数，使其使用队列进行通信
-# 这里假设可以修改 app.py，添加以下代码到 app.py 中
-# 在 app.py 顶部添加
-import threading
-import queue
-file_dialog_queue = queue.Queue()
-
-def select_file():
-    def show_dialog():
-        from tkinter import Tk, filedialog
-        root = Tk()
-        root.withdraw()
-        file_path = filedialog.askopenfilename(
-            title="选择文本或INI文件",
-            filetypes=[("Text Files", "*.txt;*.ini")]
-        )
-        file_dialog_queue.put(file_path)
-    threading.Thread(target=show_dialog).start()
-    return file_dialog_queue.get()
-
 
 # 创建一个线程来运行 app
 app_thread = threading.Thread(target=run_app)
 app_thread.daemon = True  # 将线程设置为守护线程，这样主线程退出时，子线程也会退出
 app_thread.start()
-
 
 print("启动 PyWebview 窗口...")
 window = webview.create_window(
@@ -45,6 +20,5 @@ window = webview.create_window(
     height=540
 )
 
-# 启动 PyWebview，只保留一次调用
-webview.start()
+
 webview.start()
--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import gradio as gr
-import subprocess
-from tkinter import Tk, filedialog  # 导入文件对话框库
-import tkinter as tk
 from tkinter import filedialog
-import queue
-import threading
-import main
+import wavname2lab
+from textgrid2json import ds_json2filter, word2utau_phone, TextGrid2ds_json, ds_json2word
+from json2oto import json2CV_oto, json2oto, json2VCV_oto
+from oto import oto_check
+from oto import oto_rw
+import sys
+import os
+import subprocess
+import click
 
 def run():
     demo.launch(server_port=7860, show_error=True, inbrowser=False)
@@ -14,9 +17,8 @@ def generate_config(
         wav_path, ds_dict, presamp, cut, ignore,
         VCV_mode, lab, cv_sum, vc_sum, vv_sum,
         cv_offset, vc_offset, pitch, CV_repeat,
-        VC_repeat, cover, sofa_model
-):
-    config = (
+        VC_repeat, cover, sofa_model,SOFA_mode,SOFA_type,progress=gr.Progress()):
+    config1 = (
         f"wav_path={wav_path}\n"
         f"ds_dict={ds_dict}\n"
         f"presamp={presamp}\n"
@@ -34,74 +36,109 @@ def generate_config(
         f"VC_repeat={VC_repeat}\n"
         f"cover={cover}\n"
         f"sofa_model={sofa_model}"
+        f"SOFA_mode={SOFA_mode}\n"
+        f"SOFA_type={SOFA_type}\n"
     )
     with open('config.txt', 'w', encoding='utf-8') as f:
-        f.write(config)
-
-    yield "✅ 配置文件已生成，开始执行主程序..."
-    main.auto_run('config.txt')
-    # exe_path = "TextGrid2oto.exe"
-    # try:
-    #     # 明确指定 encoding 为 utf-8
-    #     result = subprocess.run([exe_path, 'config.txt'], capture_output=True, text=True, check=True, encoding='utf-8')
-    #     yield result.stdout
-    # except subprocess.CalledProcessError as e:
-    #     error_msg = f"❌ 错误：执行 {exe_path} 时出错，返回码: {e.returncode}，错误信息: {e.stderr}"
-    #     print(error_msg)
-    #     yield error_msg
-    # # print(result.stdout)
-    #
-    # except FileNotFoundError:
-    #     # 处理文件未找到的错误
-    #     error_msg = f"❌ 错误：未找到指定的可执行文件 {exe_path}，请检查路径。"
-    #     print(error_msg)
-    #     yield error_msg
-    # except PermissionError:
-    #     # 处理权限不足的错误
-    #     error_msg = f"❌ 错误：没有执行 {exe_path} 的权限，请检查文件权限。"
-    #     print(error_msg)
-    #     yield error_msg
-    # except subprocess.CalledProcessError as e:
-    #     # 处理可执行文件执行失败的错误
-    #     error_msg = f"❌ 错误：执行 {exe_path} 时出错，返回码: {e.returncode}，错误信息: {e.stderr}"
-    #     print(error_msg)
-    #     yield error_msg
-    yield "🎉 任务完成！最终结果：..."
-
+        f.write(config1)
+        f.write(f'#python infer.py --folder {wav_path} --dictionary {os.path.abspath(ds_dict)} --ckpt {os.path.abspath(sofa_model)} --out_formats textgrid --save_confidence')
+    progress(0, desc="✅ 配置文件已生成，开始执行主程序...")
+    with open('config.txt', 'r', encoding='utf-8') as f:
+        config = f.read().split('\n')
+        for i in range(len(config)):
+            config[i] = config[i].strip()
+        # 转为字典
+        config = {config[i].split('=')[0]: config[i].split('=')[1] for i in range(len(config)) if
+                  config[i] != '' and not config[i].startswith('#')}  # 修改判断条件为跳过以#开头的行
+        # 将字符串转换为列表并将每个元素转换为float类型
+        config['cut'] = config['cut'].split(',')
+        config['cv_sum'] = [float(i) for i in config['cv_sum'].strip('[]').split(',')]
+        config['vc_sum'] = [float(i) for i in config['vc_sum'].strip('[]').split(',')]
+        config['vv_sum'] = [float(i) for i in config['vv_sum'].strip('[]').split(',')]
+        config['cv_offset'] = [float(i) for i in config['cv_offset'].strip('[]').split(',')]
+        config['vc_offset'] = [float(i) for i in config['vc_offset'].strip('[]').split(',')]
+        config['TextGrid_path'] = config['wav_path'] + '/TextGrid'
+    progress(0.1,'1.配置文件读取成功')
+    if config['lab'] == 'Y' or config['lab'] == 'y':
+        progress(0.2,'1.生成lab')
+        wavname2lab.run(config['wav_path'], config['cut'])
+    if SOFA_type == 0:
+        progress(0.3, '2.正在前往sofa生成TextGrid')
+        sys.path.append('SOFA')
+        from SOFA import infer
+        print(f'--folder {wav_path} --dictionary {os.path.abspath(ds_dict)} --ckpt {os.path.abspath(sofa_model)} --out_formats textgrid --save_confidence')
+        sys.argv = f'--folder {wav_path} --dictionary {os.path.abspath(ds_dict)} --ckpt {os.path.abspath(sofa_model)} --out_formats textgrid --save_confidence'
+        # infer.main(ckpt=os.path.abspath(sofa_model),mode='force',ap_detector='LoudnessSpectralcentroidAPDetector',g2p=os.path.abspath(ds_dict),folder=wav_path,out_formats='textgrid',in_format='lab',save_confidence=True)
+        with click.Context(infer.main) as ctx:
+            result = ctx.invoke(
+                infer.main,
+                ckpt=os.path.abspath(sofa_model),
+                folder=wav_path,
+                dictionary=os.path.abspath(ds_dict),
+                out_formats='textgrid',
+                save_confidence=True
+            )
+    VCV_mode = config['VCV_mode']
+    if not VCV_mode:
+        VCV_mode = '0'
+    progress(0.4,'3.生成json')
+    TextGrid2ds_json.run(config['TextGrid_path'])
+    ds_json2filter.run(config['ds_dict'], config['TextGrid_path'] + '/json/ds_phone.json', config['ignore'])
+    progress(0.5,'3.生成word.json')
+    ds_json2word.run(config['ds_dict'], config['TextGrid_path'] + '/json/ds_phone_filter.json')
+    progress(0.6,'6.生成oto.ini')
+    if VCV_mode == '1':
+        progress(0.6,'生成模式：VCV')
+        json2VCV_oto.run(config['presamp'], config['TextGrid_path'] + '/json/utau_phone.json',
+                         config['TextGrid_path'] + '/json/word_phone.json',
+                         config['wav_path'], config['cv_sum'], config['vc_sum'], config['vv_sum'])
+    elif VCV_mode == '2':
+        progress(0.6,'生成模式：CVV')
+        json2CV_oto.run(config['presamp'], config['TextGrid_path'] + '/json/utau_phone.json',
+                        config['TextGrid_path'] + '/json/word_phone.json',
+                        config['wav_path'], config['cv_sum'], config['vc_sum'], config['vv_sum'])
+    elif VCV_mode == '0':
+        progress(0.6,'生成模式：CVVC')
+        json2oto.run(config['presamp'], config['TextGrid_path'] + '/json/utau_phone.json',
+                     config['TextGrid_path'] + '/json/word_phone.json',
+                     config['wav_path'], config['cv_sum'], config['vc_sum'], config['vv_sum'])
+    else:
+        progress(0.6,'VCV_mode数值错误')
+        input(0.6,'退出')
+        return 'VCV_mode数值错误'
+    progress(0.7,'7.读取CV和VC oto.ini')
+    cv = oto_rw.oto_read(config['wav_path'] + '/cv_oto.ini')
+    vc = oto_rw.oto_read(config['wav_path'] + '/vc_oto.ini')
+    progress(0.7,'8.剔除重复项')
+    cv = oto_rw.oto_repeat(cv, int(config['CV_repeat']))
+    vc = oto_rw.oto_repeat(vc, int(config['VC_repeat']))
+    progress(0.7,'9.偏移oto数值.ini')
+    if config['cv_offset'] != [0.0, 0.0, 0.0, 0.0, 0.0]:
+        cv = oto_rw.oto_offset(cv, config['cv_offset'])
+        progress(0.7,'9.1.偏移CV数值,运行成功')
+    if config['vc_offset'] != [0.0, 0.0, 0.0, 0.0, 0.0]:
+        vc = oto_rw.oto_offset(vc, config['vc_offset'])
+        progress(0.7,'9.1.偏移VC数值,运行成功')
+    progress(0.8,'10.合并oto.ini')
+    oto_rw.oto_write(config['wav_path'] + '/oto.ini', cv + vc, config['pitch'], config['cover'])
+    progress(0.9,'11.检测缺少的音素')
+    oto_check.run(config['wav_path'] + '/oto.ini', config['presamp'], config['pitch'], config['VCV_mode'])
+    progress(1,"🎉 任务完成！最终结果：")
+    return "🎉 任务完成！最终结果：去命令行窗口查看"
 # 定义文件夹选择函数
 def select_folder():
-    root = Tk()
-    root.withdraw()  # 隐藏主窗口
     folder_path = filedialog.askdirectory(title="选择文件夹")  # 打开文件夹选择对话框
-    root.destroy()
     return folder_path
 
 
-# 创建一个队列用于线程间通信
-file_dialog_queue = queue.Queue()
-
 def select_file():
-    def show_dialog():
-        root = tk.Tk()
-        root.withdraw()
-        file_path = filedialog.askopenfilename(
-            title="选择文本或INI文件",
-            filetypes=[("Text Files", "*.txt;*.ini")]
-        )
-        file_dialog_queue.put(file_path)
-        root.destroy()  # 销毁 Tkinter 窗口
-
-    # 在主线程中执行 Tkinter 操作
-    if threading.current_thread() is threading.main_thread():
-        show_dialog()
-    else:
-        threading.Thread(target=show_dialog).start()
-
-    return file_dialog_queue.get()
+    file_path = filedialog.askopenfilename(
+        title="选择文本或INI文件",
+        filetypes=[("Text Files", "*.txt;*.ini")]
+    )
+    return file_path
 
 def model_file():
-    root = Tk()
-    root.withdraw()  # 隐藏主窗口
     file_path = filedialog.askopenfilename(
         title="选择模型文件",
         filetypes=[("Model Files", "*.ckpt;*.onnx")]  # 限制文件类型[4,6](@ref)
@@ -120,6 +157,22 @@ def update_params(voice_type):
     else:
         return "0,0,1.5,1,2", "3,0,2,1,2", "3,3,1.5,1,1.5", "0,0,0,0,0", "0,0,0,0,0"
 
+def scan_model_folder():
+    model_dir = "model"
+    if os.path.exists(model_dir) and os.path.isdir(model_dir):
+        sub_folders = [d for d in os.listdir(model_dir) if os.path.isdir(os.path.join(model_dir, d))]
+        return sub_folders
+    return []
+
+# 定义选择文件夹后更新 ds_dict 和 sofa_model 的函数
+def update_paths(selected_folder):
+    folder_path = os.path.join("model", selected_folder)
+    txt_files = [f for f in os.listdir(folder_path) if f.endswith('.txt')]
+    ckpt_files = [f for f in os.listdir(folder_path) if f.endswith('.ckpt')]
+    ds_dict_path = os.path.join(folder_path, txt_files[0]) if txt_files else ""
+    sofa_model_path = os.path.join(folder_path, ckpt_files[0]) if ckpt_files else ""
+    return ds_dict_path, sofa_model_path
+
 with gr.Blocks(title="UTAU 参数生成器") as demo:
     gr.Markdown("### 必填参数配置")
     with gr.Row(equal_height=True):
@@ -134,6 +187,22 @@ with gr.Blocks(title="UTAU 参数生成器") as demo:
             with gr.Column(scale=2,min_width=150):
                 presamp_btn = gr.Button("选择文件", variant="primary")
     with gr.Row(equal_height=True):
+        SOFA_mode = gr.Radio(
+            choices=[("开启", 0), ("关闭", 1)],  # (显示文本, 实际值)
+            value=0,  # 默认选中值
+            label="是否生成TextGrid"
+        )
+        SOFA_type = gr.Radio(
+            choices=[("SOFA", 0), ("HubertFA", 1)],  # (显示文本, 实际值)
+            value=0,  # 默认选中值
+            label="选择标记程序（目前仅sofa模型）"
+        )
+        # 替换原按钮为下拉框
+        model_folders = scan_model_folder()
+        model_folder_selector = gr.Dropdown(choices=model_folders, label="选择模型")
+
+
+    with gr.Row(equal_height=True):
         with gr.Row(equal_height=True):
             with gr.Column(scale=3,min_width=150):
                 ds_dict = gr.Textbox(label="sofa字典路径",placeholder="输入模型路径")
@@ -141,7 +210,7 @@ with gr.Blocks(title="UTAU 参数生成器") as demo:
                 ds_dict_btn = gr.Button("选择文件", variant="primary")
         with gr.Row(equal_height=True):
             with gr.Column(scale=3,min_width=150):
-                sofa_model = gr.Textbox(label="sofa模型路径(暂不支持)",placeholder="输入文件路径")
+                sofa_model = gr.Textbox(label="sofa模型路径",placeholder="输入文件路径")
             with gr.Column(scale=2,min_width=150):
                 model_btn = gr.Button("选择文件", variant="primary")
 
@@ -175,6 +244,11 @@ with gr.Blocks(title="UTAU 参数生成器") as demo:
             VC_repeat = gr.Textbox(label="VC重复次数", value="1")
     # 定义更新参数的函数
 
+    model_folder_selector.change(
+        fn=update_paths,
+        inputs=model_folder_selector,
+        outputs=[ds_dict, sofa_model]
+    )
     # 按钮点击事件绑定
     folder_btn.click(
         fn=select_folder,  # 调用文件夹选择函数
@@ -212,7 +286,7 @@ with gr.Blocks(title="UTAU 参数生成器") as demo:
             wav_path, ds_dict, presamp, cut, ignore,
             VCV_mode, lab, cv_sum, vc_sum, vv_sum,
             cv_offset, vc_offset, pitch, CV_repeat,
-            VC_repeat, cover,sofa_model
+            VC_repeat, cover,sofa_model,SOFA_mode,SOFA_type
         ],
         outputs=output
     )
