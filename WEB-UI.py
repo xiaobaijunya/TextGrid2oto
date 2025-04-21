c77c3799ed35c5076b6edc183fd00913a8d108aa
xiaobaijunya <xiaobaijunya@gmail.com>
2025-04-21 11:26:42 +0800

    更改窗口大小

--- WEB-UI.py
+++ WEB-UI.py
@@ -11,14 +11,14 @@ def run_app():
 app_thread = threading.Thread(target=run_app)
 app_thread.daemon = True  # 将线程设置为守护线程，这样主线程退出时，子线程也会退出
 app_thread.start()
-
+w=960*1.2
+h=640*1.2
 print("启动 PyWebview 窗口...")
 window = webview.create_window(
     title='TextGrid2oto',
     url='http://127.0.0.1:7860',
-    width=850,
-    height=540
+    width=int(w),
+    height=int(h),
 )
-
 
 webview.start()
